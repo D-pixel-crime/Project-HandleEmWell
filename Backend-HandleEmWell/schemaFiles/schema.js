a12dfed9c8b4f22f1c06a9e6c9257bfd8053a11b
D-pixel-crime <dpk4383@gmail.com>
2024-03-17 19:01:53 +0530

    Basic Schema Changes

--- Backend-HandleEmWell/schemaFiles/schema.js
+++ Backend-HandleEmWell/schemaFiles/schema.js
@@ -1,11 +1,12 @@
 import {
+  GraphQLEnumType,
   GraphQLID,
   GraphQLList,
+  GraphQLNonNull,
   GraphQLObjectType,
   GraphQLSchema,
   GraphQLString,
 } from "graphql";
-import { clients, projects } from "../sampleData.js";
 
 import { ClientSchemaModel } from "../models/Client.js";
 import { ProjectSchemaModel } from "../models/Project.js";
@@ -33,7 +34,7 @@ const ProjectType = new GraphQLObjectType({
     client: {
       type: ClientType,
       resolve(parent, args) {
-        return clients.find((element) => element.id === parent.clientId);
+        return ClientSchemaModel.findById(parent.clientId);
       },
     },
   }),
@@ -48,29 +49,105 @@ const RootQueryType = new GraphQLObjectType({
         id: { type: GraphQLID },
       },
       resolve(parent, args) {
-        return clients.find((client) => client.id === args.id);
+        return ClientSchemaModel.findById(args.id);
       },
     },
     project: {
       type: ProjectType,
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-        return projects.find((project) => project.id === args.id);
+        return ProjectSchemaModel.findById(args.id);
       },
     },
     allClients: {
       type: new GraphQLList(ClientType),
       resolve(parent, args) {
-        return clients;
+        return ClientSchemaModel.find();
       },
     },
     allProjects: {
       type: new GraphQLList(ProjectType),
       resolve(parent, args) {
-        return projects;
+        return ProjectSchemaModel.find();
       },
     },
   },
 });
 
-export const schema = new GraphQLSchema({ query: RootQueryType });
+const MutationType = new GraphQLObjectType({
+  name: "Mutation",
+  fields: {
+    addClient: {
+      type: ClientType,
+      args: {
+        name: { type: GraphQLNonNull(GraphQLString) }, // Making sure that null name is not added
+        email: { type: GraphQLNonNull(GraphQLString) },
+        phone: { type: GraphQLNonNull(GraphQLString) },
+      },
+      resolve(parent, args) {
+        const resClient = ClientSchemaModel.create({
+          name: args.name,
+          email: args.email,
+          phone: args.phone,
+        });
+        return resClient;
+      },
+    },
+    deleteClient: {
+      type: ClientType,
+      args: {
+        id: { type: GraphQLNonNull(GraphQLID) },
+      },
+      resolve(parent, args) {
+        const resDeleteClient = ClientSchemaModel.findByIdAndDelete(args.id);
+        const deleteAllClientProjects = ProjectSchemaModel.deleteMany({
+          clientId: args.id,
+        });
+        return resDeleteClient;
+      },
+    },
+
+    addProject: {
+      type: ProjectType,
+      args: {
+        name: { type: GraphQLNonNull(GraphQLString) },
+        status: {
+          type: new GraphQLEnumType({
+            name: "ProjectStatus",
+            values: {
+              new: { value: "Yet To Start" },
+              ongoing: { value: "On-Going" },
+              completed: { value: "Completed" },
+            },
+          }),
+          defaultValue: "Yet To Start",
+        },
+        description: { type: GraphQLNonNull(GraphQLString) },
+        clientId: { type: GraphQLNonNull(GraphQLID) },
+      },
+      resolve(parent, args) {
+        const resProject = ProjectSchemaModel.create({
+          name: args.name,
+          description: args.description,
+          status: args.status,
+          clientId: args.clientId,
+        });
+        return resProject;
+      },
+    },
+    deleteProject: {
+      type: ProjectType,
+      args: {
+        id: { type: GraphQLNonNull(GraphQLID) },
+      },
+      resolve(parent, args) {
+        return ProjectSchemaModel.findByIdAndDelete(args.id);
+      },
+    },
+  },
+});
+
+export const schema = new GraphQLSchema({
+  query: RootQueryType,
+  mutation: MutationType,
+});
